0c95d9030022fddda448d65e1101d134a37fea6c
Ankit <kingaka7@gmail.com>
2023-01-05 23:23:33 +0530

    big fix

--- Session One/app.js
+++ Session One/app.js
@@ -1,4 +1,24 @@
-const root = document.getElementById("root");
-const Greeting = document.createElement("h1");
-Greeting.innerHTML = "Welcome All ";
-root.appendChild(Greeting);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = (
+  <h1 id="title" key="heading1">
+    React Heading
+  </h1>
+);
+
+const heading2 = (
+  <h1 id="title2" key="heading2">
+    React Heading again too
+  </h1>
+);
+
+const container = (
+  <div>
+    {heading}
+    {heading2}
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
